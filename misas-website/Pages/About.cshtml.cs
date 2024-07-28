d8dddb63e19f43894ddd923dcc2332ef05740394
aTTomVeera <adam@veeraengineers.com>
2024-07-27 21:04:05 -0400

    initial commit

---
+++ misas-website/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace misas_website.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ misas-website/Pages/Catering.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace misas_website.Pages
+{
+    public class CateringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
